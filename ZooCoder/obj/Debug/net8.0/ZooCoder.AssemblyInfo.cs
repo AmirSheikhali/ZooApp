aaaeb2d6c8046eb1fc0386c59d2762233ff98e19
Amir_sheikhali_02 <cu7617xk@go.minnstate.edu>
2026-05-14 05:08:23 +0000

    final draft

--- ZooCoder/obj/Debug/net8.0/ZooCoder.AssemblyInfo.cs
+++ ZooCoder/obj/Debug/net8.0/ZooCoder.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZooCoder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+807444b189129a1cf3735a4d16ffbfbacdb25040")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adf97d11f9da5e63f170b36154221d8ae17574d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZooCoder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZooCoder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
